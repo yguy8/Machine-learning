3e3355c1cf9aae3bd41943c233909ac214a6ade5
Clara Jenhee <clara20franco@gmail.com>
2025-08-28 22:25:07 -0600

    car move and create sensors

--- controls.js
+++ controls.js
@@ -1,46 +1,45 @@
 class Controls{
     constructor(){
-        this.foward = false;
-        this.left = false;
-        this.right = false;
-        this.reverse = false;
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
 
         this.#addKeyboardListeners();
     }
+
     #addKeyboardListeners(){
         document.onkeydown=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
-                    this.left = true;
+                    this.left=true;
                     break;
                 case "ArrowRight":
-                    this.right = true;
+                    this.right=true;
                     break;
                 case "ArrowUp":
-                    this.forward = true;
+                    this.forward=true;
                     break;
                 case "ArrowDown":
-                    this.reverse = true;
+                    this.reverse=true;
                     break;
             }
-
         }
         document.onkeyup=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
-                    this.left = false;
+                    this.left=false;
                     break;
                 case "ArrowRight":
-                    this.right = false;
+                    this.right=false;
                     break;
                 case "ArrowUp":
-                    this.forward = false;
+                    this.forward=false;
                     break;
                 case "ArrowDown":
-                    this.reverse = false;
+                    this.reverse=false;
                     break;
             }
         }
     }
-
 }
