41a2fa610c81a674066f47db84313595d2445776
Clara Jenhee <clara20franco@gmail.com>
2025-08-29 23:18:52 -0600

    Its ready

--- network.js
+++ network.js
@@ -7,19 +7,41 @@ class NeuralNetwork{
             ));
         }
     }
+
     static feedForward(givenInputs,network){
-        let outputs = Level.feedForward(
+        let outputs=Level.feedForward(
             givenInputs,network.levels[0]);
         for(let i=1;i<network.levels.length;i++){
-            outputs = Level.feedForward(
+            outputs=Level.feedForward(
                 outputs,network.levels[i]);
         }
         return outputs;
     }
+
+    static mutate(network,amount=1){
+        network.levels.forEach(level => {
+            for(let i=0;i<level.biases.length;i++){
+                level.biases[i]=lerp(
+                    level.biases[i],
+                    Math.random()*2-1,
+                    amount
+                )
+            }
+            for(let i=0;i<level.weights.length;i++){
+                for(let j=0;j<level.weights[i].length;j++){
+                    level.weights[i][j]=lerp(
+                        level.weights[i][j],
+                        Math.random()*2-1,
+                        amount
+                    )
+                }
+            }
+        });
+    }
 }
 
 class Level{
-    constructor(inputCount, outputCount){
+    constructor(inputCount,outputCount){
         this.inputs=new Array(inputCount);
         this.outputs=new Array(outputCount);
         this.biases=new Array(outputCount);
@@ -38,6 +60,7 @@ class Level{
                 level.weights[i][j]=Math.random()*2-1;
             }
         }
+
         for(let i=0;i<level.biases.length;i++){
             level.biases[i]=Math.random()*2-1;
         }
@@ -46,18 +69,21 @@ class Level{
     static feedForward(givenInputs,level){
         for(let i=0;i<level.inputs.length;i++){
             level.inputs[i]=givenInputs[i];
-    }
+        }
+
         for(let i=0;i<level.outputs.length;i++){
-            let sum = 0
-            for(let j = 0;j<level.inputs.length;j++){
-                sum += level.inputs[j] * level.weights[j][i];
+            let sum=0
+            for(let j=0;j<level.inputs.length;j++){
+                sum+=level.inputs[j]*level.weights[j][i];
             }
+
             if(sum>level.biases[i]){
                 level.outputs[i]=1;
             }else{
                 level.outputs[i]=0;
-            }
+            } 
         }
+
         return level.outputs;
     }
 }
--- sensor.js
+++ sensor.js
@@ -14,9 +14,10 @@ class Sensor{
         this.readings=[];
         for(let i=0;i<this.rays.length;i++){
             this.readings.push(
-                this.#getReading(this.rays[i],
+                this.#getReading(
+                    this.rays[i],
                     roadBorders,
-                     traffic
+                    traffic
                 )
             );
         }
@@ -36,20 +37,21 @@ class Sensor{
                 touches.push(touch);
             }
         }
+
         for(let i=0;i<traffic.length;i++){
-            const poly =traffic[i].polygon;
+            const poly=traffic[i].polygon;
             for(let j=0;j<poly.length;j++){
-                const touch=getIntersection(
-                ray[0],
-                ray[1],
-                poly[j],
-                poly[(j+1)%poly.length]
-            );
-            if(touch){
-                touches.push(touch);
+                const value=getIntersection(
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j+1)%poly.length]
+                );
+                if(value){
+                    touches.push(value);
+                }
             }
         }
-    }
 
         if(touches.length==0){
             return null;
